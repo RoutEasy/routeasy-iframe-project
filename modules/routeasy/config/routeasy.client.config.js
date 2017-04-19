986b9a7093b3ec603d5d1e041648c6889d536a01
Victor Ferreira da Silva <victor.silva@routeasy.com.br>
2017-04-19 15:02:06 -0300

    fix url environment

--- modules/routeasy/config/routeasy.client.config.js
+++ modules/routeasy/config/routeasy.client.config.js
@@ -3,9 +3,7 @@
     angular
         .module('RouteasyIframe')
         .constant('appConfig', {
-            url: function() { return 'http://homolog-app.routeasy.com.br/'; },
+            url: function() { return 'http://app.routeasy.com.br/'; },
             urlIframe: function() { return '#!/routings/create/1?token={{TOKEN}}'; }            
         });
-
-    
 })();
